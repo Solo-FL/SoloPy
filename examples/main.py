e58fa6807706e8d08356519f00004b573592eab4
SOLO FL <70513020+Solo-FL@users.noreply.github.com>
2021-02-22 11:07:34 +0100

    Update main.py

--- examples/main.py
+++ examples/main.py
@@ -1,6 +1,6 @@
 import solo_motor_controller as Solo
 
-solo = Solo.SoloMotorController('address')
+solo = Solo.SoloMotorController(0)
 
 print('Firmware Version: ', solo.get_firmware_version())
 print('Command Mode: ',solo.set_command_mode(True))

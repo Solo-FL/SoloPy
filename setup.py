2af8430ecd9bec05f4dced97697adf5b7dfc0040
Ali Salehi <mldsalehi@hotmail.com>
2021-08-22 20:54:26 +0430

    setup.py

--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@ from distutils.core import setup
 setup(
   name = 'SoloPy',         # How you named your package folder (MyLib)
   packages = ['SoloPy'],   # Chose the same as "name"
-  version = '3.0',      # Start with a small number and increase it with every change you make
-  license='GPL',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
+  version = 'v1.0',      # Start with a small number and increase it with every change you make
+  license='gpl-3.0',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'PYTHON Library for Control Solo Motor',   # Give a short description about your library
   author = 'Solo',                   # Type in your name
   author_email = 'support@solomotorcontrollers.com',      # Type in your E-Mail
@@ -17,7 +17,7 @@ setup(
     'Development Status :: 4 - Beta',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
     'Intended Audience :: Developers',      # Define that your audience are developers
     'Topic :: Software Development :: Build Tools',
-    'License :: OSI Approved :: MIT License',   # Again, pick a license
+    'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',   # Again, pick a license
     'Programming Language :: Python :: 3',      #Specify which pyhton versions that you want to support
     'Programming Language :: Python :: 3.4',
     'Programming Language :: Python :: 3.5',

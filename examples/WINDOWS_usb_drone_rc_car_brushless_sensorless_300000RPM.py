b9d3c62fa50340ede796c8c35da0653c612929f9
SOLO Motor Controllers <70513020+Solo-FL@users.noreply.github.com>
2021-11-01 23:18:43 +0100

    Example on how to use SoloPy on Windows

---
+++ examples/WINDOWS_usb_drone_rc_car_brushless_sensorless_300000RPM.py
@@ -0,0 +1,148 @@
+#    Title: How to Drive Fast Drone or RC car Brushless Motors using WINDOWS (USB port COM3) and SOLO in Sensoless Mode
+#    Author: SOLOMOTORCONTROLLERS
+#    Date: 2021
+#    Code version: 1.0.1
+#    Availability: https://github.com/Solo-FL/SoloPy/
+#    The Motor used for Testings: 4150KV, 4x4 SCT 550
+
+from SoloPy import solo_motor_controller as solo
+import time
+import sys
+
+
+# the device address of SOLO
+__solo_address = 0
+
+# In this example, make sure you put SOLO into Closed-Loop by
+# pressing the Piano Switch NO  # 5 DOWN. in SOLO UNO
+
+# Desired Switching or PWM Frequency at Output
+pwm_frequency = 79
+
+# Motor's Number of Poles
+numberOfPoles = 2
+
+# Select the Ultarfast BLDC_PMSM motor type
+motor_type = 3
+
+# Speed controller Kp
+speed_controller_kp = 0.3
+
+# Speed controller Ki
+speed_controller_ki = 0.005
+
+# Current Limit of the Motor
+current_limit = 32.0
+
+# Battery of Bus Voltage
+bus_voltage = 0
+
+# Desired Speed Reference
+desired_motor_speed = 0
+
+# Motor speed feedback
+actual_motor_speed = 0
+
+
+def __loop():
+
+  # set the Direction on C.W.
+  __solo_driver.set_direction(0)
+
+  # set a new reference for speed[RPM]
+  desired_motor_speed = 15000
+  __solo_driver.set_speed_reference(desired_motor_speed)
+
+  # wait till motor reaches to the reference
+  time.sleep(1)
+
+  actual_motor_speed = __solo_driver.get_speed()
+  print("\n Motor Speed: \n",actual_motor_speed)
+
+  time.sleep(5)
+
+  # stop the motor
+  desired_motor_speed = 0
+  __solo_driver.set_speed_reference(desired_motor_speed)
+  time.sleep(2)
+  
+  # set the Direction on C.C.W.
+  __solo_driver.set_direction(1)
+
+  # set a new reference for speed[RPM]
+  desired_motor_speed = 30000
+  __solo_driver.set_speed_reference(desired_motor_speed)
+
+  # wait till motor reaches to the reference
+  time.sleep(1)
+
+  actual_motor_speed = __solo_driver.get_speed()
+  print("\n Motor Speed: \n",actual_motor_speed)
+  
+  time.sleep(5)
+
+  # stop the motor
+  desired_motor_speed = 0
+  __solo_driver.set_speed_reference(desired_motor_speed)
+  time.sleep(2)
+
+def __setup():
+    
+    # Initialize the SOLO object using the device address of SOLO at 0
+    global __solo_driver
+    __solo_driver = solo.SoloMotorController(__solo_address, 937500, "COM3") 
+
+    time.sleep(2)
+
+    bus_voltage = __solo_driver.get_bus_voltage()
+    print('get_bus_voltage: ',bus_voltage)
+
+    while bus_voltage <= 0:
+        # wait here till communication is established
+        bus_voltage = __solo_driver.get_bus_voltage()
+        print("\n Trying to Connect To SOLO")
+        time.sleep(1)
+
+    print("\n Communication Established succuessfully!")
+
+    # Initial Configurations
+    __solo_driver.set_pwm_frequency(pwm_frequency)
+    __solo_driver.set_current_limit(current_limit)
+
+    # select Digital Mode
+    __solo_driver.set_command_mode(1)
+
+    __solo_driver.set_motor_type(motor_type)
+
+    # run the motor identification
+    # run ID. always after selecting the Motor Type!
+    __solo_driver.set_identification(1)
+
+    print("\n Identifying the Motor")
+
+    # wait at least for 2sec till ID. is done
+    time.sleep(3)
+
+    # Operate in Sensor-less Mode
+    __solo_driver.set_speed_control_mode(0)
+
+    # Control The Speed
+    __solo_driver.set_control_mode(0)
+    
+    # Set the Number of Poles 
+    __solo_driver.set_number_of_poles(numberOfPoles)
+
+    # Speed Controller Tunings
+    __solo_driver.set_speed_controller_Kp(speed_controller_kp)
+    __solo_driver.set_speed_controller_Ki(speed_controller_ki)
+
+    while True:
+        __loop()
+
+
+def do_work():
+    __setup()
+
+
+if __name__ == "__main__":
+    do_work()
---
+++ examples/WINDOWS_usb_position_control_brushless_motor.py
@@ -0,0 +1,156 @@
+#
+#   Title: Position Control of a Brushless Motor with WINDOWS (USB port COM3) and SOLO
+#   Author: SOLOMOTORCONTROLLERS
+#   Date: 2021
+#   Code version: 1.0.0
+#   Availability: https://github.com/Solo-FL/SoloPy/
+#   The Motor used for Testings: teknic m-2310P-LN-04K
+#_________________________________________________________________________________________________
+
+# Importing SoloPy
+from SoloPy import solo_motor_controller as solo
+import time
+import sys
+
+
+# the device address of SOLO
+__solo_address = 0
+
+# For this Test, make sure you have calibrated your Encoder before
+# to know more please read: https: // www.solomotorcontrollers.com/how-to-connect-calibrate-incremental-encoder-with-solo/
+
+# Desired Switching or PWM Frequency at Output in kHz
+pwm_frequency = 70
+
+# Motor's Number of Poles
+numberOfPoles = 8
+
+# Motor's Number of Encoder Lines(PPR pre-quad)
+numberOfEncoder_lines = 1000
+
+# Select the Normal BLDC_PMSM motor type
+motor_type = 1
+
+# Speed controller Kp
+speed_controller_kp = 0.15
+
+# Speed controller Ki
+speed_controller_ki = 0.03
+
+# Position controller Kp
+position_controller_kp = 1.2
+
+# Position controller Ki
+position_controller_ki = 0.02
+
+# Current Limit of the Motor
+current_limit = 15.0
+
+# Battery or Bus Voltage
+bus_voltage = 0
+
+# Desired Speed Limit[RPM]
+desired_speed_limit = 3000
+
+# Desired Position Reference
+desired_position_reference = 0
+
+# Motor speed feedback
+actual_motor_speed = 0
+
+# Motor position feedback
+actual_motor_position = 0
+
+
+def __loop():
+  # set a desired Speed Limit for trajectory in RPM
+  desired_speed_limit = 5000;
+  __solo_driver.set_speed_limit(desired_speed_limit);
+  
+  # set a positive desired Position Reference 
+  desired_position_reference =+500000;
+  __solo_driver.set_desired_position(desired_position_reference);
+
+  # wait till motor reaches to the reference 
+  time.sleep(3); 
+
+  actual_motor_position = __solo_driver.get_encoder_position()
+  print("\n Number of Pulses passed: \n",actual_motor_position)
+
+
+  # set a desired Speed Limit for trajectory in RPM
+  desired_speed_limit = 1500
+  __solo_driver.set_speed_limit(desired_speed_limit)
+  
+  # set a negative desired Position Reference 
+  desired_position_reference =-32559
+  __solo_driver.set_desired_position(desired_position_reference)
+
+  # wait till motor reaches to the reference 
+  time.sleep(6)
+
+  actual_motor_position = __solo_driver.get_encoder_position()
+  print("\n Number of Pulses passed: \n",actual_motor_position)
+
+def __setup():
+    # Initialize the SOLO object using the device address of SOLO at 0
+    global __solo_driver
+    __solo_driver = solo.SoloMotorController(__solo_address, 937500, "COM3")
+
+    time.sleep(2)
+
+    bus_voltage = __solo_driver.get_bus_voltage()
+    while bus_voltage <= 0:
+        # wait here till communication is established
+        bus_voltage = __solo_driver.get_bus_voltage()
+        print("\n Trying to Connect To SOLO")
+        time.sleep(1)
+
+    print("\n Communication Established succuessfully!")
+
+    # Initial Configurations
+    __solo_driver.set_pwm_frequency(pwm_frequency)
+    __solo_driver.set_current_limit(current_limit)
+    __solo_driver.set_encoder_lines(numberOfEncoder_lines)
+
+    # select Digital Mode
+    __solo_driver.set_command_mode(1)
+
+    __solo_driver.set_motor_type(motor_type)
+
+    # run the motor identification
+    # run ID. always after selecting the Motor Type!
+    __solo_driver.set_identification(1)
+
+    print("\n Identifying the Motor")
+
+    # wait at least for 2sec till ID. is done
+    time.sleep(3)
+
+   #Operate while using Quadrature Encoder
+    __solo_driver.set_speed_control_mode(1)
+
+   #Control The Position
+    __solo_driver.set_control_mode(2)
+    
+    # Set the Number of Poles 
+    __solo_driver.set_number_of_poles(numberOfPoles)
+
+    # Speed Controller Tunings
+    __solo_driver.set_speed_controller_Kp(speed_controller_kp)
+    __solo_driver.set_speed_controller_Ki(speed_controller_ki)
+
+    #Position Controller Tunings
+    __solo_driver.set_position_controller_kp(position_controller_kp)
+    __solo_driver.set_position_controller_ki(position_controller_ki)
+
+    while True:
+        __loop()
+
+
+def do_work():
+    __setup()
+
+
+if __name__ == "__main__":
+    do_work()

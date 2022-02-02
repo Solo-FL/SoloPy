15100cd0a25240c86d47df0d57434948d7f53b34
SOLO Motor Controllers <70513020+Solo-FL@users.noreply.github.com>
2022-02-02 22:29:59 +0100

    v2.0

--- SoloPy/constant.py
+++ SoloPy/constant.py
@@ -4,110 +4,116 @@ BroadcastAddress = 0xFF
 ENDING = 0xFE
 ERROR = 0xEE  # 0xEEEEEEEE
 CRC = 0x00
-WriteAddress = 0x01
+WriteDeviceAddress = 0x01
 WriteCommandMode = 0x02
 WriteCurrentLimit = 0x03
-WriteTorqueReference = 0x04
+WriteTorqueReferenceIq = 0x04
 WriteSpeedReference = 0x05
 WritePowerReference = 0x06
-WriteIdentification = 0x07
-WriteStopSystem = 0x08
-WritePWMFrequency = 0x09
+WriteMotorParametersIdentification = 0x07
+WriteEmergencyStop = 0x08
+WriteOutputPwmFrequencyKhz = 0x09
 WriteSpeedControllerKp = 0x0A
 WriteSpeedControllerKi = 0x0B
-WriteDirection = 0x0C
-WriteResistance = 0x0D
-WriteInductance = 0x0E
-WriteNumberOfPoles = 0x0F
-WriteEncoderLines = 0x10
+WriteMotorDirection = 0x0C
+WriteMotorResistance = 0x0D
+WriteMotorInductance = 0x0E
+WriteMotorPolesCounts = 0x0F
+WriteIncrementalEncoderLines = 0x10
 WriteSpeedLimit = 0x11
 WriteResetAddress = 0x12
-WriteSpeedControlMode = 0x13
-WriteResetToFactory = 0x14
+WriteFeedbackControlMode = 0x13
+WriteResetFactory = 0x14
 WriteMotorType = 0x15
 WriteControlMode = 0x16
 WriteCurrentControllerKp = 0x17
 WriteCurrentControllerKi = 0x18
 WriteMonitoringMode = 0x19
-WriteMagnetizingCurrentReference = 0x1A
-WriteDesiredPosition = 0x1B
+WriteMagnetizingCurrentIdReference = 0x1A
+WritePositionReference = 0x1B
 WritePositionControllerKp = 0x1C
 WritePositionControllerKi = 0x1D
 WriteResetPositionToZero = 0x1F  # Home
-WriteOverwriteTheErrors = 0x20
-# Set Sensorless Observer Gain for Normal Brushless Motor
-WriteGainNormalBrushless = 0x21
+WriteOverwriteErrorRegister = 0x20
+# Set Sensorless Observer Gain for Normal BLDC-PMSM Motors
+WriteObserverGainBldcPmsm = 0x21
 # Set Sensorless Observer Gain for Ultra-Fast Brushless Motor
-WriteGainUltraFastBrushless = 0x22
-WriteGainDC = 0x23  # Set Sensorless Observer Gain for DC Motor
+WriteObserverGainBldcPmsmUltrafast = 0x22
+# Set Sensorless Observer Gain for DC Motor
+WriteObserverGainDc = 0x23  
 # Set Sensorless Observer Filter Gain for Normal Brushless Motor
-WriteFilterGainNormalBrushless = 0x24
+WriteFilterGainBldcPmsm = 0x24
 # Set Sensorless Observer Filter Gain for ultra-fast Brushless Motor
-WriteFilterGainUltraFastBrushless = 0x25
-WriteUartBaudRate = 0x26  # Set UART line baud-rate - 937500 / 115200 [ bits/s]
-WriteStartENCHallCalibration =  0x27
-WriteSetENCHallCCWOffset =  0x28
-WriteSetENCHallCWOffset =  0x29
+WriteFilterGainBldcPmsmUltrafast = 0x25
+# Set UART line baud-rate - 937500 / 115200 [ bits/s]
+WriteUartBaudRate = 0x26  
+WriteSensorCalibration = 0x27
+WriteEncoderHallCcwOffset = 0x28
+WriteEncoderHallCwOffset = 0x29
 WriteSpeedAccelerationValue = 0x2A
 WriteSpeedDecelerationValue = 0x2B
-WriteCanBusBaudRate = 0x2
+WriteCanBusBaudRate = 0x2C
 
-ReadAddress = 0x81
-ReadVoltageA = 0x82
-ReadVoltageB = 0x83
-ReadCurrentA = 0x84
-ReadCurrentB = 0x85
+ReadDeviceAddress = 0x81
+ReadPhaseAVoltage = 0x82
+ReadPhaseBVoltage = 0x83
+ReadPhaseACurrent = 0x84
+ReadPhaseBCurrent = 0x85
 ReadBusVoltage = 0x86
-ReadIM = 0x87
-ReadVM = 0x88
+ReadDcMotorCurrentIm = 0x87
+ReadDcMotorVoltageVm = 0x88
 ReadSpeedControllerKp = 0x89
 ReadSpeedControllerKi = 0x8A
-ReadPWMFrequency = 0x8B
+ReadOutputPwmFrequencyHz = 0x8B
 ReadCurrentLimit = 0x8C
-ReadQuadratureCurrent = 0x8D
-ReadDirectCurrent = 0x8E  # Magnetizing
-ReadNumberOfPoles = 0x8F
-ReadEncoderLine = 0x90
+ReadQuadratureCurrentIqFeedback = 0x8D
+ReadMagnetizingCurrentIdFeedback = 0x8E  # Magnetizing 
+ReadMotorPolesCounts = 0x8F
+ReadIncrementalEncoderLines = 0x90
 ReadCurrentControllerKp = 0x91
 ReadCurrentControllerKi = 0x92
-ReadTemperature = 0x93
-ReadResistance = 0x94
-ReadInductance = 0x95
-ReadSpeed = 0x96
+ReadBoardTemperature = 0x93
+ReadMotorResistance = 0x94
+ReadMotorInductance = 0x95
+ReadSpeedFeedback = 0x96
 ReadMotorType = 0x97
-# TODO: 0x98 !?
-ReadSpeedControlMode = 0x99
+# TODO: 0x98
+ReadFeedbackControlMode = 0x99
 ReadCommandMode = 0x9A
 ReadControlMode = 0x9B
 ReadSpeedLimit = 0x9C
 ReadPositionControllerKp = 0x9D
 ReadPositionControllerKi = 0x9E
-# TODO: 0x9F !?
-ReadEncoderPosition = 0xA0
+# TODO: 0x9F 
+ReadPositionCountsFeedback = 0xA0
 ReadErrorRegister = 0xA1
-ReadFirmwareVersion = 0xA2
-ReadHardwareVersion = 0xA3
-ReadTorque = 0xA4  # Read Torque /“Iq” Reference
+ReadDeviceFirmwareVersion = 0xA2
+ReadDeviceHardwareVersion = 0xA3
+ReadTorqueReferenceIq = 0xA4  # Read Torque /“Iq” Reference
 ReadSpeedReference = 0xA5  # Read Speed Reference
-ReadMagnetizingCurrent = 0xA6  # Read Magnetizing Current / “Id” Reference
+ReadMagnetizingCurrentIdReference = 0xA6  # Read Magnetizing Current / “Id” Reference
 ReadPositionReference = 0xA7
 ReadPowerReference = 0xA8
-ReadDirectionRotation = 0xA9
-# Read the Non-linear observer Gain for Normal Brushless motor in Sensorless mode
-ReadGainNormalBrushless = 0xAA
-# Read the Non-linear observer Gain for Ultra-fast Brushless motor in Sensorless mode
-ReadGainUltraFastBrushless = 0xAB
-ReadGainDC = 0xAC  # Read the Non-linear observer Gain for DC motor in Sensorless mode
-# Read the Non-linear observer Filter Gain for Normal Brushless motor in Sensorless mode
-ReadFilterGainNormalBrushless = 0xAD
-# Read the Non-linear Filter Gain for Ultra-fast Brushless motor in Sensorless mode
-ReadFilterGainUltraFastBrushless = 0xAE
-Read3PhaseMotorAngle = 0xB0 # Read Estimated or Measured Rotor Angle
-ReadENCHallCCWOffset = 0xB1
-ReadENCHallCWOffset  = 0xB2
+ReadMotorDirection = 0xA9
+# Read Sensorless Observer Gain for Normal BLDC-PMSM Motors
+ReadObserverGainBldcPmsm = 0xAA
+# Read Sensorless Observer Gain for Ultra-fast BLDC-PMSM Motors
+ReadObserverGainBldcPmsmUltrafast = 0xAB
+# Read Observer Gain for DC Motor
+ReadObserverGainDc = 0xAC  
+# Read Sensorless Observer Filter Gain for Normal BLDC-PMSM Motors
+ReadFilterGainBldcPmsm  = 0xAD
+# Read Sensorless Observer Filter Gain for Ultra Fast BLDC-PMSM Motors
+ReadFilterGainBldcPmsmUltrafast= 0xAE
+# Read Motor’s Angle
+Read3PhaseMotorAngle = 0xB0  
+ReadEncoderHallCcwOffset = 0xB1
+ReadEncoderHallCwOffset = 0xB2
 ReadUartBaudRate = 0xB3  # 0 / 1 ( 937500 / 115200 [bits/s] )
 ReadSpeedAccelerationValue = 0xB4
 ReadSpeedDecelerationValue = 0xB5
 ReadCanBusBaudRate = 0xB6
+ReadEncoderIndexCounts = 0xB8
 
 InputOutOfRange = "input out of range, set function will not be executed"
+InputNeedEnum = "the input is an enum, function will not be executed"

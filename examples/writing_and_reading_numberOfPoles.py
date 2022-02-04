f486b091598451fbd1e230621b53c5d6c1004bcc
SOLO Motor Controllers <70513020+Solo-FL@users.noreply.github.com>
2022-02-04 11:02:42 +0100

    v2.4

--- examples/writing_and_reading_numberOfPoles.py
+++ examples/writing_and_reading_numberOfPoles.py
@@ -1,48 +1,14 @@
 # EXAMPLE of how to set the Motor number of poles,
 # every second we repit the setting and the reading of it
 
-import time
-import sys
-
 #Importing SoloPy
-from SoloPy import solo_motor_controller as solo
+import SoloPy as solo
+import time
 
-# the device address of SOLO
-__solo_address = 0 
+# Initialize the SOLO object 
+mySolo = solo.SoloMotorController(port="COM3")
 
-# Motor's Number of Poles
-__numberOfPoles_write = 4
-numberOfPoles_read = 0
-
-
-def __loop():
-    #Setting
-    __solo_driver.set_number_of_poles(__numberOfPoles_write)
-
-    #Reading
-    number_of_poles = __solo_driver.get_number_of_poles()
-
-    # Print
-    print("\n Read from SOLO -> Number Of Poles: \n")
-    print(number_of_poles)
-
+while True:
+    mySolo.set_motor_poles_counts(4)
+    print("Read from SOLO -> Number Of Poles: " + str(mySolo.get_motor_poles_counts()))
     time.sleep(1)
-
-
-def __setup():
-    #Initialize the SOLO object using the device address of SOLO at 0
-    global __solo_driver
-    __solo_driver = solo.SoloMotorController(__solo_address)
-    while True:
-        __loop()
-
-def do_work():
-    __setup()
-
-
-if __name__ == "__main__":
-    do_work()
-
-
-
-

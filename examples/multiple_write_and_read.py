e4bbabe8c27c6d0a69286a69c7c5e539f8973055
SOLO Motor Controllers <70513020+Solo-FL@users.noreply.github.com>
2022-02-04 11:09:14 +0100

    v2.4

--- examples/multiple_write_and_read.py
+++ examples/multiple_write_and_read.py
@@ -1,83 +1,22 @@
-# Importing SoloPy
-from SoloPy import solo_motor_controller as solo
+#Importing SoloPy
+import SoloPy as solo
+
 import time
-import sys
 
-# **********************************************/
-# You can instanciate up to 254 SOLOs in a network
-# Each SOLO in the network should have a unique address
+# Initialize the SOLO object 
+mySolo = solo.SoloMotorController()
 
-
-__solo_address1 = 0  # the device address of SOLO
-__solo_address2 = 1  # the device address of SOLO
-# *********************************************/
-
-__pWMFrequency_write = 20  # Desired Switching Frequency at Output
-__numberOfPoles_write = 4  # Set the Motor's Number of Poles
-__encoderLines_write = 2000  # Set PPR for the Encoder
-
-busVoltage_read = 0  # Battery or Supply Input Voltage
-temperature_read = 0  # SOLO board Temperature
-voltageA_read = 0  # Phase A voltage reading (3phase)
-inductance_read = 0  # Motor Phase Inductance
-pWMFrequency_read = 0  # Read Switching Frequency of SOLO
-numberOfPoles_read = 0  # Read the Motor's Number of Poles
-encoderLines_read = 0  # Read the PPR set for the Encoder
-
-
-def __loop():
-    # Setting Some Parameters
-    __solo_driver1.set_pwm_frequency(__pWMFrequency_write)
-    __solo_driver1.set_number_of_poles(__numberOfPoles_write)
-    __solo_driver1.set_encoder_lines(__encoderLines_write)
+while True:
+     # Setting Some Parameters
+    mySolo.set_pwm_frequency(20)
+    mySolo.set_number_of_poles(4)
+    mySolo.set_encoder_lines(2000)
 
     # Reading Some Parameters
-    busVoltage_read = __solo_driver1.get_bus_voltage()
-    temperature_read = __solo_driver1.get_temperature()
-    voltageA_read = __solo_driver1.get_voltage_a()
-    inductance_read = __solo_driver1.get_inductance()
-    pWMFrequency_read = __solo_driver1.get_Pwm_frequency()
-    numberOfPoles_read = __solo_driver1.get_number_of_poles()
-    encoderLines_read = __solo_driver1.get_encoder_lines()
-
-    print("\n List Of some parameters read from SOLO")
-
-    # Print
-    print("\n Read from SOLO -> BusVoltage: \n")
-    print(busVoltage_read)
-
-    print("\n Read from SOLO -> Temperature: \n")
-    print(temperature_read)
-
-    print("\n Read from SOLO -> VoltageA: \n")
-    print(voltageA_read)
-
-    print("\n Read from SOLO -> Inductance: \n")
-    print(inductance_read)
-
-    print("\n Read from SOLO -> PWMFrequency: \n")
-    print(pWMFrequency_read)
-
-    print("\n Read from SOLO -> NumberOfPoles: \n")
-    print(numberOfPoles_read)
-
-    print("\n Read from SOLO -> EncoderLines: \n")
-    print(encoderLines_read)
-
-    time.sleep(1)
-
-
-def __setup():
- # Initialize the SOLO object using the device address of SOLO at 0
-    global __solo_driver1
-    __solo_driver1 = solo.SoloMotorController(__solo_address1)
-    while True:
-        __loop()
-
-
-def do_work():
-    __setup()
-
-
-if __name__ == "__main__":
-    do_work()
+    print("List Of some parameters read from SOLO")
+    print("Read from SOLO -> BusVoltage: " + str(mySolo.get_bus_voltage()))
+    print("Read from SOLO -> Temperature: " + str(mySolo.get_board_temperature()))
+    print("Read from SOLO -> VoltageA: "+  str(mySolo.get_phase_a_voltage()))
+    print("Read from SOLO -> PWMFrequency: "+  str(mySolo.get_output_pwm_frequency_khz()))
+    print("Read from SOLO -> NumberOfPoles: "+  str(mySolo.get_motor_poles_counts()))
+    print("Read from SOLO -> EncoderLines: "+  str(mySolo.get_incremental_encoder_lines()))

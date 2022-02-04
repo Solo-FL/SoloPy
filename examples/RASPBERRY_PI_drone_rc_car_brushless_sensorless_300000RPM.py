2e064d24dc1054f63cc292afd7476d4f88d799a2
SOLO Motor Controllers <70513020+Solo-FL@users.noreply.github.com>
2022-02-04 02:24:03 +0100

    v2.4

--- examples/RASPBERRY_PI_drone_rc_car_brushless_sensorless_300000RPM.py
+++ examples/RASPBERRY_PI_drone_rc_car_brushless_sensorless_300000RPM.py
@@ -1,155 +1,81 @@
 #    Title: How to Drive Fast Drone or RC car Brushless Motors using RASPBERRY-PIand SOLO in Sensoless Mode
 #    Author: SOLOMOTORCONTROLLERS
 #    Date: 2021
-#    Code version: 1.0.1
+#    Code version: 2.0.0
 #    Availability: https://github.com/Solo-FL/SoloPy/
 #    Please make sure you are applying the right wiring between SOLO and your RASPBERRY-PI
 #    The Code below has been tested on RASPBERRY-PI 4B
 #    The Motor used for Testings: 4150KV, 4x4 SCT 550
 #   2Importing SOLO PYTHON RASPBERRY-PI library
 
-from SoloPy import solo_motor_controller as solo
+#Importing SoloPy
+import SoloPy as solo
+
 import time
 import sys
-
-
-# the device address of SOLO
-__solo_address = 0
 
 # In this example, make sure you put SOLO into Closed-Loop by
 # pressing the Piano Switch NO  # 5 DOWN. in SOLO UNO
 
-# ____________________________________________________________________
-# High Speed High Performance Baudrate (Recommended)
-# Use this baudrate to have the best and real performance
-# of SOLO under all conditions 937500;
-baudrate = 937500
-
-# Low Speed Low Performance Baudrate
-# Use this baudrate only for devices that don't support
-# 937500 or 921600 baudrates.
-#baudrate = 115200
-# _____________________________________________________________________
-
-# Desired Switching or PWM Frequency at Output
-pwm_frequency = 79
-
-# Motor's Number of Poles
-numberOfPoles = 2
-
-# Select the Ultarfast BLDC_PMSM motor type
-motor_type = 3
-
-# Speed controller Kp
-speed_controller_kp = 0.3
-
-# Speed controller Ki
-speed_controller_ki = 0.005
-
-# Current Limit of the Motor
-current_limit = 32.0
-
-# Battery of Bus Voltage
-bus_voltage = 0
-
-# Desired Speed Reference
-desired_motor_speed = 0
-
-# Motor speed feedback
-actual_motor_speed = 0
-
-
 def __loop():
 
-  # set the Direction on C.W.
-  __solo_driver.set_direction(0)
-
-  # set a new reference for speed[RPM]
-  desired_motor_speed = 15000
-  __solo_driver.set_speed_reference(desired_motor_speed)
+  mySolo.set_motor_direction(solo.DIRECTION.CLOCKWISE)
+  mySolo.set_speed_reference(15000)
 
   # wait till motor reaches to the reference
   time.sleep(1)
 
-  actual_motor_speed = __solo_driver.get_speed()
-  print("\n Motor Speed: \n",actual_motor_speed)
-
+  print("Motor Speed: "+ str(mySolo.get_speed_feedback()))
   time.sleep(5)
 
   # stop the motor
-  desired_motor_speed = 0
-  __solo_driver.set_speed_reference(desired_motor_speed)
+  mySolo.set_speed_reference(0)
   time.sleep(2)
   
-  # set the Direction on C.C.W.
-  __solo_driver.set_direction(1)
-
-  # set a new reference for speed[RPM]
-  desired_motor_speed = 30000
-  __solo_driver.set_speed_reference(desired_motor_speed)
+  mySolo.set_motor_direction(solo.DIRECTION.COUNTERCLOCKWISE)
+  mySolo.set_speed_reference(30000)
 
   # wait till motor reaches to the reference
   time.sleep(1)
 
-  actual_motor_speed = __solo_driver.get_speed()
-  print("\n Motor Speed: \n",actual_motor_speed)
-  
+  print("Motor Speed: "+ str(mySolo.get_speed_feedback()))
   time.sleep(5)
 
   # stop the motor
-  desired_motor_speed = 0
-  __solo_driver.set_speed_reference(desired_motor_speed)
+  mySolo.set_speed_reference(0)
   time.sleep(2)
 
 def __setup():
+    global mySolo
+
+    # Initialize the SOLO object 
+    mySolo = solo.SoloMotorController()
     
-    # Initialize the SOLO object using the device address of SOLO at 0
-    global __solo_driver
-    __solo_driver = solo.SoloMotorController(__solo_address, baudrate)
-
-    time.sleep(2)
-
-    bus_voltage = __solo_driver.get_bus_voltage()
-    print('get_bus_voltage: ',bus_voltage)
-
-    while bus_voltage <= 0:
-        # wait here till communication is established
-        bus_voltage = __solo_driver.get_bus_voltage()
-        print("\n Trying to Connect To SOLO")
+    # wait here till communication is established
+    while mySolo.serial_is_working() == False:
         time.sleep(1)
-
-    print("\n Communication Established succuessfully!")
+    print("Communication Established succuessfully!")
 
     # Initial Configurations
-    __solo_driver.set_pwm_frequency(pwm_frequency)
-    __solo_driver.set_current_limit(current_limit)
-
-    # select Digital Mode
-    __solo_driver.set_command_mode(1)
-
-    __solo_driver.set_motor_type(motor_type)
+    mySolo.set_command_mode(solo.COMMAND_MODE.DIGITAL)
+    mySolo.set_motor_type(solo.MOTOR_TYPE.BLDC_PMSM_ULTRAFAST)
+    mySolo.set_output_pwm_frequency_khz(79)
+    mySolo.set_current_limit(32.0)
 
     # run the motor identification
     # run ID. always after selecting the Motor Type!
-    __solo_driver.set_identification(1)
-
     print("\n Identifying the Motor")
-
+    mySolo.motor_parameters_identification(solo.ACTION.START)
     # wait at least for 2sec till ID. is done
     time.sleep(3)
 
-    # Operate in Sensor-less Mode
-    __solo_driver.set_speed_control_mode(0)
-
-    # Control The Speed
-    __solo_driver.set_control_mode(0)
-    
-    # Set the Number of Poles 
-    __solo_driver.set_number_of_poles(numberOfPoles)
+    mySolo.set_control_mode(solo.CONTROL_MODE.SPEED_MODE)
+    mySolo.set_feedback_control_mode(solo.FEEDBACK_CONTROL_MODE.SENSOR_LESS)
+    mySolo.set_motor_poles_counts(2)
 
     # Speed Controller Tunings
-    __solo_driver.set_speed_controller_Kp(speed_controller_kp)
-    __solo_driver.set_speed_controller_Ki(speed_controller_ki)
+    mySolo.set_speed_controller_kp(0.3)
+    mySolo.set_speed_controller_ki(0.005)
 
     while True:
         __loop()

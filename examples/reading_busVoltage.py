116c433b296f3f845c337b6d1b943c0f7dcc2bdb
SOLO Motor Controllers <70513020+Solo-FL@users.noreply.github.com>
2022-02-04 10:59:41 +0100

    v2.4

--- examples/reading_busVoltage.py
+++ examples/reading_busVoltage.py
@@ -1,39 +1,11 @@
 # EXAMPLE of how read the SOLO Battery or Supply Input Voltage,
 # every second we print the value of the BusVoltage
 
-import time
-import sys
-
 #Importing SoloPy
-from SoloPy import solo_motor_controller as solo
+import SoloPy as solo
+import time
 
-# the device address of SOLO
-__solo_address = 0
- 
-busVoltage = 0
-
-def __loop():
-    #Reading
-    busVoltage = __solo_driver.get_bus_voltage()
-
-    # Print
-    print("\n Read from SOLO -> BusVoltage: \n")
-    print(busVoltage)
-
+mySolo = solo.SoloMotorController()
+while True:
+    print("Read from SOLO -> BusVoltage: " + str(mySolo.get_bus_voltage()))
     time.sleep(1)
-
-
-
-def __setup():
- #Initialize the SOLO object using the device address of SOLO at 0
-    global __solo_driver
-    __solo_driver = solo.SoloMotorController(__solo_address)
-    while True:
-        __loop()
-
-def do_work():
-    __setup()
-
-
-if __name__ == "__main__":
-    do_work()
